a6496734e8387a9c428792e7fa4d589df1a0f14b
TanerGur <gurtaner_79@yahoo.com>
2018-06-15 02:46:22 -0400

    MentorArslan

---
+++ eclipse-workspace/MavenMentoring/src/main/java/com/indeed/Calculator.java
@@ -0,0 +1,20 @@
+package com.indeed;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Calculator {
+	
+	public static void main(String[] args) {
+		
+		
+		WebDriverManager.chromedriver().setup();
+	
+
+	 WebDriver driver = new ChromeDriver();
+	
+	driver.get("https://cnn.com");
+}
+}
